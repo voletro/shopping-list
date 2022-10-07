bec8eaaddca2f8df298d113cee558705ad115f1d
voletro <voletro@gmail.com>
2022-10-07 20:57:32 +1100

    Search Bar

--- src/screens/tab/search.screen.tsx
+++ src/screens/tab/search.screen.tsx
@@ -1,44 +1,28 @@
+import { useNavigation } from "@react-navigation/native";
 import { Input, Icon, Row } from "native-base";
-import { MagnifyingGlass } from "phosphor-react-native";
-import { View, TouchableOpacity, Text } from "react-native";
+import { Barcode, MagnifyingGlass } from "phosphor-react-native";
+import { View, TouchableOpacity, Text, TextInput } from "react-native";
 import { SafeArea } from "../../components/safe-area.component";
 
 export const SearchBar = () => {
+  const navigation = useNavigation();
   return (
-    <View style={{ marginTop: 10 }}>
-      <View className="rounded-lg mb-2 border-gray-200 border-[1px] bg-gray-50 w-[95%]">
-        <Row className="">
-          <View className="m-3">
-            <MagnifyingGlass />
-          </View>
-          <Input
-            variant="unstyled"
-            placeholder="Search Products"
-            className=""
-          />
-        </Row>
-      </View>
-      <Input
-        placeholder="Search Products"
-        bg="#fff"
-        _android={{
-          _focus: { borderColor: "muted.300" },
-        }}
-        _ios={{
-          _focus: { borderColor: "muted.300" },
-        }}
-        width="95%"
-        className="py-3 px-1 rounded-md text-lg w-[95%]"
-        InputLeftElement={
-          <Icon
-            m="2"
-            ml="3"
-            size="6"
-            color="muted.300"
-            as={<MagnifyingGlass />}
-          />
-        }
-      />
+    <View className="rounded-lg mb-2 border-gray-200 border-[1px] bg-gray-50 w-[95%] mt-2 justify-between">
+      <Row className="w-11/12">
+        <View className="m-3">
+          <MagnifyingGlass />
+        </View>
+        <TextInput
+          placeholder="Search Products"
+          className="rounded-md text-lg w-[80%]"
+        />
+        <TouchableOpacity
+          className="m-3"
+          onPress={() => navigation.navigate("BarcodeScreen")}
+        >
+          <Barcode />
+        </TouchableOpacity>
+      </Row>
     </View>
   );
 };
